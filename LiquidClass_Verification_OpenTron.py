734024e94ada8780f9b9955adfcb943049f3cff2
hayleymcc <101218862+hayleymcc@users.noreply.github.com>
2025-06-14 16:15:11 -0700

    Add files via upload

---
+++ LiquidClass_Verification_OpenTron.py
@@ -0,0 +1,121 @@
+from opentrons import types
+
+metadata = {
+    'protocolName': 'Liquid Class Verification',
+    'author': 'Hayley McCausland',
+}
+
+requirements = {
+    "robotType": "Flex",
+    "apiLevel": "2.21"
+}
+
+########################
+
+def add_parameters(parameters):
+    parameters.add_bool(variable_name="wavelength_ref",
+                        display_name="Reference Wavelength",
+                        description=" ",
+                        default=False
+                        )
+ 
+def run(ctx):
+
+    global wavelength_ref
+
+    global assay_labware  
+    global diluent_labware  
+    global reagent_labware 
+
+    global num_col_assay_plate
+    global vol_diluent
+    global vol_reagent
+
+
+    wavelength_ref = ctx.params.wavelength_ref
+
+    assay_labware = ctx.params.assay_labware 
+    diluent_labware = ctx.params.diluent_labware 
+    reagent_labware = ctx.params.reagent_labware
+
+    #################################
+
+     ## total columns to pipet to in assay plate 
+    num_col_assay_plate = 11
+    
+    ## volume of diluent and reagent to add 
+    vol_diluent = 180
+    vol_reagent = 20
+
+    # deck layout
+    reader = ctx.load_module("absorbanceReaderV1", "D3")
+
+    assay_labware = ctx.load_labware('nest_96_wellplate_200ul_flat', 'A1', 'ASSAY PLATE')
+    diluent_labware = ctx.load_labware('axygen_1_reservoir_90ml', 'A2', 'DILUENT RESERVOIR') 
+    reagent_labware = ctx.load_labware('nest_12_reservoir_15ml', 'B1', 'REAGENT RESERVOIR')
+
+    ctx.load_trash_bin('A3')
+
+    tips_200 = ctx.load_labware('opentrons_flex_96_tiprack_200ul', 'D1', 'P200 TIPS')
+    tips_50 = ctx.load_labware('opentrons_flex_96_tiprack_50ul', 'D2', 'P50 TIPS')
+
+    # pipettes
+    left_pipette = protocol.load_instrument(
+        "flex_8channel_1000", mount="left", tip_racks=[tiprack]
+    )
+
+
+
+    # protocol 
+
+    ## add diluent
+    for i, col in enumerate(range(2, 13)):  # i goes from 0 to 10
+        assay_well = f"A{col}"
+        tip_well = f"A{i+1}"  # Starts at A1 for the first transfer
+
+        left_pipette.pick_up_tip(tips_200[tip_well])               # Pick up a new tip starting from A1
+        left_pipette.aspirate(180, diluent_labware["A1"])          # Always aspirate from A1
+        left_pipette.dispense(180, assay_labware[assay_well])      # Dispense into A2-A12
+        left_pipette.drop_tip()                                  
+
+    ## add reagent and mix
+    for i, col in enumerate(range(2, 13)):  # i goes from 0 to 10
+        assay_well = f"A{col}"
+        tip_well = f"A{i+1}"  # Starts at A1 for the first transfer
+        reagent_well = f"A{i+1}"  # Starts at A1 for the first transfer
+
+        left_pipette.pick_up_tip(tips_50[tip_well])               # Pick up a new tip starting from A1
+        left_pipette.aspirate(20, reagent_labware[reagent_well])          # Always aspirate from A1
+        left_pipette.dispense(20, assay_labware[assay_well])      # Dispense into A2-A12
+        left_pipette.mix(5, 100)
+        left_pipette.drop_tip()                                  
+
+    ## read plate   
+    reader.close_lid()
+
+    if wavelength_ref:
+        reader.initialize('single', [562], reference_wavelength=650)      
+    else: reader.initialize('single', [562])
+
+    reader.open_lid()
+    working_plate = ctx.load_labware('corning_96_wellplate_360ul_flat', 'C2')
+    ctx.move_labware(labware = working_plate,
+                     new_location = reader,
+                     use_gripper=True
+                    ) 
+    reader.close_lid()
+    
+    result = reader.read(export_filename="output.csv")
+
+    reader.open_lid()
+    ctx.move_labware(labware = working_plate, 
+                     new_location = 'C2', 
+                     use_gripper=True
+                    )
+    reader.close_lid()
+
+    ##output data
+
+
+
+    
